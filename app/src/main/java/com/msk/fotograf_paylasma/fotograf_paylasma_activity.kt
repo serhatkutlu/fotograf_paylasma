cfada00706cb38e8c63be4e9e1098331d476cd2d
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2021-12-07 11:01:17 +0300

    menu butonları ve fotograf paylasma aktivitesi olusturuldu

---
+++ app/src/main/java/com/msk/fotograf_paylasma/fotograf_paylasma_activity.kt
@@ -0,0 +1,11 @@
+package com.msk.fotograf_paylasma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fotograf_paylasma_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fotograf_paylasma)
+    }
+}
--- app/src/main/java/com/msk/fotograf_paylasma/haber_activity.kt
+++ app/src/main/java/com/msk/fotograf_paylasma/haber_activity.kt
@@ -1,11 +1,38 @@
 package com.msk.fotograf_paylasma
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class haber_activity : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_haber)
+        auth= FirebaseAuth.getInstance()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater=menuInflater
+        inflater.inflate(R.menu.options,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId==R.id.resim_paylas){
+            val intent=Intent(this,fotograf_paylasma_activity::class.java)
+            startActivity(intent)
+        }
+        if (item.itemId==R.id.cikis) {
+            auth.signOut()
+             val intent=Intent(this,kullanici_activity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
